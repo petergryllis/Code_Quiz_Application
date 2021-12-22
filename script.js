ecef896efcd64df7d8976c77512102e0f7aab757
Peter Gryllis <petergryllis@riacci.com>
2021-12-22 20:24:16 +1100

    add variables for questions and answers

---
+++ script.js
@@ -0,0 +1,9 @@
+//set variables with questions, booleans and answers
+var Quesiton1 = "Which command console logs the first element of the variable game in an array?";
+var Answer1 = ["console.log(game)", "console.log(game[1])", "console.log(game[0])", "console.log(game[2])"];
+var Question2 = "How do you console log the multiplication of two numbers?";
+var Answer2 = ["console.log(a x b)", "console.log(a . b)", "console.log(a * b)", "console.log(a % b)"];
+var Question3 = "Which expression evaluates if two or more expresions are both true otherwise fale";
+var Answer3 = ["console.log(expression1 >> expression2)", "console.log(expression1 && expression2", "console.log(expression1 ** expression2)", "console.log(expression1 %% expression2"];
+var Question4 = "which variable is available to any other function in the global scope?";
+var Answer4 = ["", "", "", ""];
