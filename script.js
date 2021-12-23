807dd68b04a5fba18a50f6c869e96ca02abf1d8c
Peter Gryllis <petergryllis@riacci.com>
2021-12-23 11:58:55 +1100

    update functions for right answers to Quiz

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+//Set DOM elements
+var timerElement = document.querySelector(".");
+
+
+
+
 //set variables with questions and answers (as arrays)
 var Quesiton1 = "Which command console logs the first element of the variable game in an array?";
 var Answer1 = ["console.log(game)", "console.log(game[1])", "console.log(game[0])", "console.log(game[2])"];
 var Question2 = "How do you console log the multiplication of two numbers?";
-var Answer2 = ["console.log(a x b)", "console.log(a . b)", "console.log(a * b)", "console.log(a % b)"];
-var Question3 = "Which expression evaluates if two or more expresions are both true otherwise fale";
+var Answer2 = ["console.log(a .. b)", "console.log(a . b)", "console.log(a * b)", "console.log(a % b)"];
+var Question3 = "Which expression evaluates if two or more expresions are both true otherwise false";
 var Answer3 = ["console.log(expression1 >> expression2)", "console.log(expression1 && expression2", "console.log(expression1 ** expression2)", "console.log(expression1 %% expression2"];
 var Question4 = "which variable is available to any other function in the global scope?";
 var Answer4 = ["var dog;", "function pet () { var dog...}", "function airplane () { var flightprofile...}"];
+
 
 //set variables for correct answer, timer and timer count
 var isFinishedQuiz = true;
@@ -20,12 +27,42 @@ function init() {
     getPrevious();
 }
 
-//time function
-function startQuiz (){
-    isFinishedQuiz = true;
-    timerCount = 75;
-    //prevent start button from being clicked when round is in play
-    startbutton.disabled = true;
-    renderQuestions()
-    startTimer()
+//Set function to set timer and reduce timeCount if answer is wrong until clock  is zero
+var x = 60;
+var y = 15;
+Answer1 = false;
+Answer2 = false;
+Answer3 = false;
+Answer4 = false;
+
+function setTimer () {
+    timer = setInterval(function () {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (Answer1 === false && timerCount >= 15) {
+            timerCount = x - y;
+            timerCount.push(timer);
+        }
+        if (Answer1 === true && timerCount <= 15){
+            timerCount.push(timer);
+
+
+        }
+    }
+        
+     {
+
+     })
+
+
+}
+
+
+
+//finished game function called when all questions are answered and timer finished
+function finishedGame () {
+    wordblank.textContent = "You have answered all the questions and your score is " + Score();
+    winCounter++
+    startbutton.disabled = false;
+
 }
